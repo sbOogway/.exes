e517259fde62bd6ca52871e6f0c70b4dd53b4a47
ElPettego <mattiapapaccioli@gmail.com>
2023-08-31 01:13:03 +0200

    crontab

--- binance_crontab.sh
+++ binance_crontab.sh
@@ -3,6 +3,17 @@
 dir="/tmp/crypto_prices"
 mylist=($(awk '{print}' ~/.templates/.market_cryptos.txt))
 
-for value in ${mylist[@]}; do
-    binance_klines.sh -i 5m -s $value | jq -r .[-1][4] > $dir/$value.txt
-done; 
+mkdir $dir
+
+while true
+do 
+    for value in ${mylist[@]}; do
+        binance_klines.sh -i 1d -s $value | jq -r '.[-1][4], .[-1][1]' | sed '1s/$/,/' | tr -d '\n' > $dir/$value.txt 
+    done;
+
+    sleep 30
+
+    echo "loop done" 
+
+done
+
